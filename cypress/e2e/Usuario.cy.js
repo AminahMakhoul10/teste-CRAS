6bf206cc72ebd6ed101a6567c3f52101b6a6c61a
Aminah Makhoul <aminahmakhoul@gmail.com>
2023-11-02 14:51:38 -0300

    Fazendo alterações necessárias no cadastro de usuario e testando erros

--- cypress/e2e/Usuario.cy.js
+++ cypress/e2e/Usuario.cy.js
@@ -1,45 +1,79 @@
-const { it } = require("mocha");
+//SE DER ERRO EM ALGUM TESTE, PARE DE RODAR E RODE NOVAMENTE, NA MINHA MÁQUINA FUNCIONA
 
-describe("Cadastro Usuário", () => {
+import fake from "faker-br";
+
+describe("Login", () => {
     beforeEach(() => {
-        cy.visit("https://front-cras.app.fslab.dev/")
-        cy.get("#email").type("dev@gmail.com")
-        cy.get("#senha").type("123")
-        
-    })
+        cy.visit("https://front-cras.app.fslab.dev/");
+        cy.get("#email").type("aminahm@gmail.com");
+        cy.get("#senha").type("@Aminah12345678");
+        cy.get(".styles_button__dr0t2").click();
+    });
 
-    it("Deve realizar cadastro de um usuário com status ativo - cenário de sucesso", () =>{
-       
-        cy.visit("https://front-cras.app.fslab.dev/usuarios/listar")
-        cy.get(":nth-child(5) > .styles_button__dr0t2").click()
-        cy.get("#nomeCadastrar").type("TesteCrasAminah");
-        cy.get("#emailCadastrar").type("TesteCrasAminah@gmail.com");
-        cy.get("#senhaCadastrar").type("@TesteCrasAminah12345678");
+    const nomeFaker = fake.name.firstName()
+
+    it('Deve realizar realizar cadastro de um usuário com status ativo - cenário de sucesso', () => {
+        cy.get(".styles_buttonMenu__mmyUS > img").click();
+        cy.visit('https://front-cras.app.fslab.dev/usuarios/listar')
+        cy.get(":nth-child(5) > .styles_button__dr0t2").click();
+        cy.get("#nomeCadastrar").type(nomeFaker);
+        cy.get("#emailCadastrar").type(fake.internet.email());
+        cy.get("#senhaCadastrar").type(fake.internet.password() + "@12345678");
         cy.get('#unidadeCadastrar').select('CRAS - Vilhena')
         cy.get('#grupoCadastrar').select('Administrador')
         cy.get('[type="submit"]').click();
+
         cy.get('.Toastify__toast-body').contains('Usuário cadastrado com sucesso');
     })
 
-    it("Deve retornar as mensagem de validação ao cadastrar usuário  - cenário de insucesso", () => {
+    it('Deve retornar as mensagem de validação ao cadastrar usuário  - cenário de insucesso', () => {
 
+        cy.get(".styles_buttonMenu__mmyUS > img").click();
+        cy.visit('https://front-cras.app.fslab.dev/usuarios/listar')
+        cy.get(":nth-child(5) > .styles_button__dr0t2").click();
+        cy.get("#nomeCadastrar").type(nomeFaker);
+        cy.get("#emailCadastrar").type("TesteCrasAminah@gmail.com");
+        cy.get("#senhaCadastrar").type(fake.internet.password() + "Am@212345678");
+        cy.get('#unidadeCadastrar').select('CRAS - Vilhena')
+        cy.get('#grupoCadastrar').select('Administrador')
+        cy.get('[type="submit"]').click();
+
+        cy.get('.Toastify__toast-body').contains('E-mail já cadastrado!');
     })
 
-    it("Deve retornar uma consulta do usurário cadastrado com status ativo - cenário de sucesso", () => {
-
+    it("Deve retornar uma consulta do usuário cadastrado com status ativo - cenário de sucesso", () => {
+        cy.get(".styles_buttonMenu__mmyUS > img").click();
+        cy.visit("https://front-cras.app.fslab.dev/usuarios/listar");
+        cy.get("#nome").type(nomeFaker);
+        cy.get(":nth-child(4) > .styles_button__dr0t2").click();
+        cy.get(" tbody > :nth-child(1) > :nth-child(1)").contains(nomeFaker);
+        cy.get("tbody > :nth-child(1) > :nth-child(4)").contains("Sim");
     })
 
-    it("Deve atualizar os dados de um usuário - cenário de sucesso" , () => {
-
-    })
-
-    it("Deve visualizar as informações de um usuário - cenário de sucesso" , () => {
-        
-    })
-
+    it("Deve atualizar os dados de um usuário - cenário de sucesso", () => {
     
-  
-  });
-  
- 
-  
+        cy.get(".styles_buttonMenu__mmyUS > img").click();
+        cy.visit("https://front-cras.app.fslab.dev/usuarios/listar");
+        cy.get("#nome").type(nomeFaker);
+        cy.get(":nth-child(4) > .styles_button__dr0t2").click();
+        cy.wait(1000)
+        cy.get('[alt="Atualizar usuário"]').click();
+        cy.get('#grupo').select('Padrão')
+        cy.get('[type="submit"]').click();
+
+        cy.get('.Toastify__toast-body > :nth-child(2)').contains('Usuário atualizado com sucesso');
+    })
+
+     it("Deve visualizar as informações de um usuário - cenário de sucesso", () => {
+
+        cy.get(".styles_buttonMenu__mmyUS > img").click();
+        cy.visit("https://front-cras.app.fslab.dev/usuarios/listar");
+        cy.get("#nome").type(nomeFaker);
+        cy.get(":nth-child(4) > .styles_button__dr0t2").click();
+        cy.wait(1000)
+        cy.get('[alt="Informação do usuário"]').click();
+        cy.get(':nth-child(2) > .styles_formItem__H8I19 > .styles_container__NSLBw > .styles_text__68fzG').contains('Padrão');
+     })
+
+})
+
